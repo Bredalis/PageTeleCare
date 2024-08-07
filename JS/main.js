8c5ffe87a9925684c4460064e13fd060016d2140
PRO X <bredalisgautreaux@gmail>
2024-08-07 20:34:35 +0200

    feat: ✨ Nuevas funcionalidades y estilos

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+
+// Ocultar el indice de contenido al estar sobre footer
+document.addEventListener("scroll", function() {
+    const enlaces = document.querySelector(".contenido-enlaces");
+    const footer = document.querySelector("footer");
+    const footerTop = footer.getBoundingClientRect().top;
+    const enlacesHeight = enlaces.offsetHeight;
+
+    footerTop < window.innerHeight ? enlaces.style.opacity = "0" : enlaces.style.opacity = "1";
+});
